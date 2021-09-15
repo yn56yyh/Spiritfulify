e872b1f2e6b81b0e3128c1f8fe6fc4b42e0cf307
Nathan Yong <90749638+takeeggnathan@users.noreply.github.com>
2021-09-15 23:38:12 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+function openFunction() {
+  document.getElementById("menu").style.width = "300px";
+  document.getElementById("mainbox").style.marginLeft = "300px";
+}
+function closeFunction() {
+  document.getElementById("menu").style.width = "0px";
+  document.getElementById("mainbox").style.marginLeft = "0px";
+}
+
+//Scroll to top Javascript
+var scrollToTopBtn = document.querySelector(".scrollToTopBtn");
+var rootElement = document.documentElement;
+
+function handleScroll() {
+  // Do something on scroll
+  var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
+  if (rootElement.scrollTop / scrollTotal > 0.8) {
+    // Show button
+    scrollToTopBtn.classList.add("showBtn");
+  } else {
+    // Hide button
+    scrollToTopBtn.classList.remove("showBtn");
+  }
+}
+
+function scrollToTop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+scrollToTopBtn.addEventListener("click", scrollToTop);
+document.addEventListener("scroll", handleScroll);
+
+//Login/Signup validation
+
+(function () {
+  "use strict";
+  window.addEventListener(
+    "load",
+    function () {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName("needs-validation");
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener(
+          "submit",
+          function (event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add("was-validated");
+          },
+          false
+        );
+      });
+    },
+    false
+  );
+})();
